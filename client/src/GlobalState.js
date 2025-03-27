217a39b4e827c5ff405397e4f90c3c7193f5c337
Soumen sarkar <soumensarkar7498@gmail.com>
2025-03-27 15:19:56 +0530

    Fix refreshToken issue

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -6,6 +6,9 @@ import UserAPI from "./API/UserAPI";
 export const GlobalState = createContext();
 
 //const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL =
+  process.env.REACT_APP_API_URL || "http://localhost:4000";
 
 export const DataProvider = ({ children }) => {
   const [token, setToken] = useState(false);
