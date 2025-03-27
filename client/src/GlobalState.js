543bf817a7ea5a2331c74110cdd549543bc7bc0a
Soumen sarkar <soumensarkar7498@gmail.com>
2025-03-27 16:47:06 +0530

    Fix CORS issue

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -18,7 +18,7 @@ export const DataProvider = ({ children }) => {
       console.log("Attempting to refresh token...");
 
       const res = await axios.post(
-        "/",
+        "/user/refresh_token",
         {},
         {
           headers: {
