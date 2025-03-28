e74d0da5ba217d4f897029c5568b22d3e17228e4
Soumen sarkar <soumensarkar7498@gmail.com>
2025-03-28 08:05:49 +0530

    Fix refreshToken

--- client/src/API/ProductAPI.js
+++ client/src/API/ProductAPI.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import publicAxios from "../utils/publicAxios";
 
 function ProductAPI() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("/api/products", { withCredentials: false}) // talking to your backend
+    publicAxios
+      .get("/api/products") // ✅ no credentials sent
       .then((res) => {
-        console.log("API Response:", res.data);
+        console.log("Product API Response:", res.data);
         setProducts(res.data);
       })
-      .catch((error) => console.error("Error fetching products:", error));
+      .catch((error) =>
+        console.error("Error fetching products:", error.response?.data || error)
+      );
   }, []);
 
   return { products };
