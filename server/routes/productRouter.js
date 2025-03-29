0f6a869068123cf2553efd526dee8c4705b578c7
Soumen sarkar <soumensarkar7498@gmail.com>
2025-03-29 08:19:21 +0530

    improved productRouter

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,15 +1,7 @@
 const router = require("express").Router();
 //const productControl = require("../controllers/productControl");
 const axios = require("axios");
-// router
-//   .route("/products")
-//   .get(productControl.getProducts)
-//   .post(productControl.createProduct);
 
-// router
-//   .route("/products/:id")
-//   .delete(productControl.deleteProduct)
-//   .put(productControl.updateProduct);
 
 router.get("/products", async (req, res) => {
   try {
