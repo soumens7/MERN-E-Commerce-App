757395ee801bb9f03b735d4ac8dcf6b05fcf3589
Soumen sarkar <soumensarkar7498@gmail.com>
2025-04-08 12:35:26 +0530

    Updated middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,13 +3,16 @@ const jwt = require("jsonwebtoken"); // Importing JWT library
 const auth = (req, res, next) => {
   try {
     // Get token from the request header
-    const token = req.header("Authorization");
+    let token = req.header("Authorization");
 
     // 🔹 Debugging: Log received token
     console.log("Received Token:", token);
 
     // If token is missing, return an error
     if (!token) return res.status(400).json({ msg: "No token provided!" });
+    if (token?.startsWith("Bearer ")) {
+      token = token.slice(7);
+    }
 
     // Verify the token using JWT and secret key
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
