b69d544f186b15282b648b599fa6ae6b97e6d852
Soumen sarkar <soumensarkar7498@gmail.com>
2025-04-11 22:19:11 +0530

    Updated login page

--- client/src/components/mainpages/login/login.js
+++ client/src/components/mainpages/login/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import "./login.css";
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
 
 function Login() {
