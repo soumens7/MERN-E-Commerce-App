81626298551551a9117f9eabb6afbe69f5d2c915
Soumen sarkar <soumensarkar7498@gmail.com>
2025-04-22 17:33:27 +0530

    Improved frontend performance

--- client/src/components/mainpages/utils/ProductList/ProductList.js
+++ client/src/components/mainpages/utils/ProductList/ProductList.js
@@ -1,13 +1,11 @@
 import React, { memo } from "react";
-import { FixedSizeList as List } from "react-window"; 
 import BtnRender from "./BtnRender";
 
-// ProductCard component to render individual product
-const ProductCard = ({ product, isAdmin, addCart }) => {
+const ProductList = ({ product, isAdmin, addCart }) => {
   return (
     <div className="product_card">
       {isAdmin && <input type="checkbox" checked={product.checked} />}
-      <img src={product.image} alt={product.title} loading="lazy" />
+      <img src={product.image} alt={product.title} loading="lazy"/>
       <div className="product_box">
         <h2>{product.title}</h2>
         <span>${product.price}</span>
@@ -19,27 +17,5 @@ const ProductCard = ({ product, isAdmin, addCart }) => {
   );
 };
 
-const ProductList = ({ products, isAdmin, addCart }) => {
-  const renderRow = ({ index, style }) => {
-    const product = products[index];
-    return (
-      <div className="product_card" style={style}>
-        <ProductCard product={product} isAdmin={isAdmin} addCart={addCart} />
-      </div>
-    );
-  };
-
-  return (
-    <List
-      height={500} 
-      itemCount={products.length} 
-      itemSize={250} 
-      width={1000} 
-    >
-      {renderRow}
-    </List>
-  );
-};
-
-
+// Wrap the component with React.memo
 export default memo(ProductList);
